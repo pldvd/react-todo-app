9bdd8ff66e4ea49e5023164059b124b7b882932f
David Pal <paldavid2030@gmail.com>
2019-07-03 15:56:36 +0300

    add About component to the project

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a practice page created with React, by following this video: <a target="blank">https://www.youtube.com/watch?v=sBws8MSXN7A</a></p>
+    </React.Fragment>
+  )
+}
+
+export default About;
